ec5ae8de72eb62f1014f08bee3c6e58b5c618107
NaNeNo <nattapong.netnu@gmail.com>
2018-08-08 01:14:17 +0700

    Added my project

---
+++ config/express.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var morgan = require('morgan');
+var compression = require('compression');
+var bodyParser = require('body-parser');
+var sass = require('node-sass-middleware');
+var validator = require('express-validator');
+var cookieSession = require('cookie-session');
+
+module.exports = function() {
+    var app = express();
+
+    //Check Production or development ------------------>
+    if (process.env.NODE_ENV === 'development') {
+        //Use morgan debugging
+        app.use(morgan('dev'));
+    }
+    else {
+        //Compress Respond
+        app.use(compression);
+    }
+    // ------------------------------------------------->
+
+    //Include Cookie-session
+    app.use(cookieSession({
+        name: 'session',
+        keys: ['secret_key1', 'secret_key2']
+    }));
+
+    app.use(bodyParser.urlencoded({
+        extended: true
+    }));
+    app.use(bodyParser.json());
+    // Include Express validator
+    app.use(validator());
+
+    //Include Jade frameworks
+    app.set('views', './app/views');
+    app.set('view engine', 'jade');
+
+    //Include index route
+    require('../app/routes/index.routes')(app);
+    require('../app/routes/user.routes')(app);
+
+    //Include SASS
+    app.use(sass({
+        src: './sass',
+        dest: './public/css',
+        outputStyle: 'compressed',
+        prefix: '/css',
+        indentedSyntax: true, //For SASS
+        debug: true
+    }))
+
+    //This is load static file such as png css
+    app.use(express.static('./public'));
+
+    return app;
+};
