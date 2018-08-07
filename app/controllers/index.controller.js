c359e628852698dd9e794f54d79a4660d2e2e7f4
NaNeNo <nattapong.netnu@gmail.com>
2018-08-08 02:20:28 +0700

    Edit index.controller

--- app/controllers/index.controller.js
+++ app/controllers/index.controller.js
@@ -2,12 +2,12 @@ exports.render = function(req, res) {
     var isLoggedIn = false;
     var title = 'Hello World';
 
-    //Check cookis sessions ----------------------------------->
+    //Check cookis sessions ------------------------------------>
     if (typeof req.session.remember !== 'undefined') {
         isLoggedIn = req.session.remember;
         title = title + req.session.email;
     }
-    // -------------------------------------------------------->
+    // --------------------------------------------------------->
 
     res.render('index', {
         'title': title,
